4a40a1423ba43cf79f7dc985a8a989e0c8b018fa
Markus Fröschle <mfro@mubf.de>
2019-03-11 22:09:49 +0100

    implement popup logic

--- nvlang.c
+++ nvlang.c
@@ -40,9 +40,31 @@ static void set_nvram(struct NVM *buffer)
 	NVMaccess( 1, 0, sizeof(*buffer), buffer );
 }
 
-static void do_popup(OBJECT *tree, short originator)
+static short do_popup(OBJECT *popup, OBJECT *dial, short originator)
 {
+	short x, y;
+	short button, state;
+	short exit_obj;
 
+	wind_update(BEG_UPDATE);
+	objc_offset(dial, originator, &x, &y);
+	popup[ROOT].ob_x = x - dial[PANEL].ob_width / 2;
+	popup[ROOT].ob_y = y - dial[PANEL].ob_height / 2;
+
+	form_dial(FMD_START, 0, 0, 0, 0,
+              popup[PANEL].ob_x, popup[PANEL].ob_y,
+			  popup[PANEL].ob_width, popup[PANEL].ob_height);
+	objc_draw(popup, PANEL, MAX_DEPTH,
+	          popup->ob_x, popup->ob_y,
+			  popup->ob_width, popup->ob_height);
+	exit_obj = form_do(popup, ROOT) & 0x7fff;
+	popup[exit_obj].ob_state &= ~OS_SELECTED;
+
+	form_dial(FMD_FINISH, 0, 0, 0, 0,
+	          popup[PANEL].ob_x, popup[PANEL].ob_y,
+			  popup[PANEL].ob_width, popup[PANEL].ob_height);
+	wind_update(END_UPDATE);
+	return exit_obj;
 }
 
 int main(int argc, char *argv[])
@@ -84,6 +106,7 @@ int main(int argc, char *argv[])
 				GRECT r = { nvselect->ob_x - 2, nvselect->ob_y - 2,
 	     		            nvselect->ob_width + 4, nvselect->ob_height + 4 };
 		
+				wind_update(BEG_UPDATE);
 				form_dial(FMD_START, 0, 0, 0, 0,
 				                     r.g_x, r.g_y, r.g_w, r.g_h);
 				form_dial(FMD_GROW, 0, 0, 2, 2,
@@ -95,11 +118,26 @@ int main(int argc, char *argv[])
 				exitobj = form_do(nvselect, ROOT) & 0x7fff;
 				while (exitobj != OK && exitobj != CANCEL)
 				{
+					short ind;
+
 					if (exitobj == LANG)
-						do_popup(nvselect, LANG);
+					{
+						ind = do_popup(popup, nvselect, LANG);
+						nvselect[LANG].ob_spec.free_string = popup[ind].ob_spec.free_string;
+						nvselect[LANG].ob_state &= ~OS_SELECTED;
+						objc_draw(nvselect, LANG, MAX_DEPTH,
+						          nvselect->ob_x, nvselect->ob_y,
+								  nvselect->ob_width, nvselect->ob_height);
+					}
 					else if (exitobj == KBD_LANG)
-						do_popup(nvselect, KBD_LANG);
-					
+					{
+						ind = do_popup(popup, nvselect, KBD_LANG);
+						nvselect[KBD_LANG].ob_spec.free_string = popup[ind].ob_spec.free_string;
+						nvselect[KBD_LANG].ob_state &= ~OS_SELECTED;
+						objc_draw(nvselect, KBD_LANG, MAX_DEPTH,
+						          nvselect->ob_x, nvselect->ob_y,
+								  nvselect->ob_width, nvselect->ob_height);
+					}
 
 					exitobj = form_do(nvselect, ROOT) & 0x7fff;
 				}
@@ -109,7 +147,7 @@ int main(int argc, char *argv[])
 				form_dial(FMD_SHRINK, nvselect->ob_x, nvselect->ob_y,
 				                      nvselect->ob_width, nvselect->ob_height,
 									  0, 0, 2, 2);
-				
+				wind_update(END_UPDATE);
 				nvselect[exitobj].ob_state &= ~OS_SELECTED;
 				break;	
 				
