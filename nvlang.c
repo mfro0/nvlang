6e95784e7159fb2dc8eaaed88f555ac1608d252f
Markus Fröschle <mfro@mubf.de>
2019-03-12 19:37:08 +0100

    add up and down button strings

--- nvlang.c
+++ nvlang.c
@@ -68,30 +68,46 @@ static short do_popup(OBJECT *popup, OBJECT *dial, short originator)
 	short x, y;
 	short button, state;
 	short exit_obj;
+	OBJECT *o = &popup[ROOT];
+	const int num_items = 5;
+
+	char upstr[] = " \x01 ";
+	char dnstr[] = " \x02 ";
 
 	wind_update(BEG_UPDATE);
 	objc_offset(dial, originator, &x, &y);
-	popup[ROOT].ob_x = (x + dial[originator].ob_width / 2) - popup[ROOT].ob_width / 2;
-	popup[ROOT].ob_y = (y + dial[originator].ob_height / 2) - popup[ROOT].ob_height / 2;
 
-    GRECT sz = ob_size(popup, ROOT);
 
+	o->ob_x = (x + dial[originator].ob_width / 2) - o->ob_width / 2;
+	o->ob_y = (y + dial[originator].ob_height / 2) - o->ob_height / 2;
+
+	short first;
+	short last;
+	char *first_str;
+	char *last_str;
+
+	first = o->ob_head;
+	last = o->ob_tail;
+	first_str = popup[first].ob_spec.free_string;
+	last_str = popup[last].ob_spec.free_string;
+
+	popup[first].ob_spec.free_string = upstr;
+	popup[last].ob_spec.free_string = dnstr;
+	
 	form_dial(FMD_START, 0, 0, 0, 0,
-              sz.g_x, sz.g_y, sz.g_w, sz.g_h);
-
-    // printf("obj = { %d, %d, %d, %d }\r\n", popup->ob_x, popup->ob_y, popup->ob_width, popup->ob_height);
-    // printf("sz  = { %d, %d, %d, %d }\r\n", sz.g_x, sz.g_y, sz.g_w, sz.g_h);
+              o->ob_x, o->ob_y,
+			  o->ob_width, o->ob_height);
 
 	objc_draw(popup, ROOT, MAX_DEPTH,
-	          popup->ob_x, popup->ob_y,
-			  popup->ob_width, popup->ob_height);
+	          o->ob_x, o->ob_y,
+			  o->ob_width, o->ob_height);
 
 	exit_obj = form_do(popup, ROOT) & 0x7fff;
 	popup[exit_obj].ob_state &= ~OS_SELECTED;
 
 	form_dial(FMD_FINISH, 0, 0, 0, 0,
-              sz.g_x, sz.g_y,
-              sz.g_w, sz.g_h);
+              o->ob_x, o->ob_y,
+              o->ob_width, o->ob_height);
 
 	wind_update(END_UPDATE);
 
@@ -138,12 +154,10 @@ int main(int argc, char *argv[])
 				form_center(nvselect, &nvselect->ob_x, &nvselect->ob_y,
 				                      &nvselect->ob_width, &nvselect->ob_height);
 	
-				GRECT r = { nvselect->ob_x - 2, nvselect->ob_y - 2,
-	     		            nvselect->ob_width + 4, nvselect->ob_height + 4 };
-		
 				wind_update(BEG_UPDATE);
 				form_dial(FMD_START, 0, 0, 0, 0,
-				                     r.g_x, r.g_y, r.g_w, r.g_h);
+				                     nvselect->ob_x, nvselect->ob_y,
+									 nvselect->ob_width, nvselect->ob_height);
 				form_dial(FMD_GROW, 0, 0, 2, 2,
 				                    nvselect->ob_x, nvselect->ob_y,
 									nvselect->ob_width, nvselect->ob_height);
@@ -185,7 +199,8 @@ int main(int argc, char *argv[])
 				}
 
 				form_dial(FMD_FINISH, 0, 0, 0, 0,
-				                      r.g_x, r.g_y, r.g_w, r.g_h);
+				                      nvselect->ob_x, nvselect->ob_y,
+									  nvselect->ob_width, nvselect->ob_height);
 				form_dial(FMD_SHRINK, nvselect->ob_x, nvselect->ob_y,
 				                      nvselect->ob_width, nvselect->ob_height,
 									  0, 0, 2, 2);
