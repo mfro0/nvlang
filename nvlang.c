2851ea499ca3e525fe28c0a684fcf6b038cc08d0
Markus Fröschle <markus.froeschle@mahle.com>
2019-03-13 08:53:26 +0100

    a little cleanup

--- nvlang.c
+++ nvlang.c
@@ -20,9 +20,6 @@ struct NVM
 	unsigned char scsi;
 };
 
-static const char *lang[] = { "en", "de", "fr", "XX", "es", "it", NULL };
-static const char *KEYB[] = { "us", "de", "fr", "gb", "es", "it", "se", "ch-fr", "ch-de", NULL };
-
 #define NVLANG_RSC "nvlang.rsc"
 
 static void reset_nvram(struct NVM *buffer)
@@ -37,30 +34,7 @@ static void get_nvram(struct NVM *buffer)
 
 static void set_nvram(struct NVM *buffer)
 {
-	NVMaccess( 1, 0, sizeof(*buffer), buffer );
-}
-
-static GRECT ob_size(OBJECT *tree, short obj)
-{
-    short x, y;
-    short width;
-    short height;
-    short framesize;
-
-    x = tree[obj].ob_x;
-    y = tree[obj].ob_y;
-    width = tree[obj].ob_width;
-    height = tree[obj].ob_height;
-    framesize = tree[obj].ob_spec.obspec.framesize;
-
-    x = framesize > 0 ? x - framesize : x;
-    y = framesize > 0 ? y - framesize : y;
-    width = framesize > 0 ? width + framesize * 2 : width;
-    height = framesize > 0 ? height + framesize * 2 : height;
-
-    GRECT rect = { x, y, width, height };
-
-    return rect;
+	NVMaccess(1, 0, sizeof(*buffer), buffer);
 }
 
 static short do_popup(OBJECT *popup, OBJECT *dial, short originator)
@@ -144,12 +118,14 @@ void do_dialog(void)
 				
 	objc_draw(nvselect, ROOT, MAX_DEPTH, nvselect->ob_x, nvselect->ob_y,
 	          nvselect->ob_width, nvselect->ob_height);
+
 	exitobj = form_do(nvselect, ROOT) & 0x7fff;
-	while (exitobj != OK && exitobj != CANCEL)
+
+	while (OK != exitobj && CANCEL != exitobj)
 	{
 		short ind;
 
-		if (exitobj == LANG)
+		if (LANG == exitobj)
 		{
 			ind = do_popup(popup, nvselect, LANG);
 			nvselect[LANG].ob_spec.free_string = popup[ind].ob_spec.free_string;
@@ -160,7 +136,7 @@ void do_dialog(void)
 
             nvm.language = ind - ENGLISH_US;
 		}
-		else if (exitobj == KBD_LANG)
+		else if (KBD_LANG == exitobj)
 		{
 			ind = do_popup(popup, nvselect, KBD_LANG);
 			nvselect[KBD_LANG].ob_spec.free_string = popup[ind].ob_spec.free_string;
@@ -174,7 +150,7 @@ void do_dialog(void)
 
 		exitobj = form_do(nvselect, ROOT) & 0x7fff;
 
-        if (exitobj == OK)
+        if (OK == exitobj)
             set_nvram(&nvm);
 	}
 
@@ -184,7 +160,9 @@ void do_dialog(void)
 	form_dial(FMD_SHRINK, nvselect->ob_x, nvselect->ob_y,
 	          nvselect->ob_width, nvselect->ob_height,
 			  0, 0, 2, 2);
+
 	wind_update(END_UPDATE);
+
 	nvselect[exitobj].ob_state &= ~OS_SELECTED;
 }
 
@@ -210,7 +188,6 @@ int main(int argc, char *argv[])
     if (!_app)
     {
 	    menu_register(apid, "  NVLANG");
-
 
 	    while (1)
 	    {
